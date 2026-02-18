8716e2332b412a7a6d5445d802f560d80238332a
Khongmeng Kormoua <khongmengkormoua@gmail.com>
2026-02-18 12:38:21 -0600

    add finished image manipulation

--- jupyter/week02/image_inspector.m
+++ jupyter/week02/image_inspector.m
@@ -5,21 +5,23 @@ close all;
 % Read images
 Img_RAW = imread('HW_toyobjects_Gray/HW_toyobjects_Gray.bmp');
 Img_JPG = imread('HW_toyobjects_Gray/HW_toyobjects_index.jpg');
-Img_BMP = imread(['HW_toyobjects_Gray/HW_toyobjects_index.bmp']);
+[Img_BMP, map] = imread('HW_toyobjects_Gray/HW_toyobjects_index.bmp');
 
-% Display side-by-side
 figure;
 
 subplot(1,3,1);
 imshow(Img_RAW);
+axis on
 title('Raw Grayscale');
 
 subplot(1,3,2);
 imshow(Img_JPG);
+axis on
 title('JPEG Image');
 
 subplot(1,3,3);
-imshow(Img_BMP);
+imshow(Img_BMP, map);
+axis on
 title('BMP Image');
 
 % Enable interactive tools
